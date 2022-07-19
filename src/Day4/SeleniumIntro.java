dbded2917b9256e5fb154b82e253aa9ad4b614e7
hmk10 <halitmk@gmail.com>
2022-07-18 21:49:36 -0400

    Selenium Intro - Navigating to Website

---
+++ src/Day4/SeleniumIntro.java
@@ -0,0 +1,27 @@
+package Day4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+
+    public static void main(String[] args) {
+
+        // Define driver type and driver path (location)
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+
+        // Create a new WebDriver object
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize browser window
+        driver.manage().window().maximize();
+
+        // Navigate to website
+        driver.get("https://techno.study/");
+
+        // Quit the browser
+        driver.quit();
+
+    }
+
+}
